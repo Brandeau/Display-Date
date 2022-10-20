de7ec7705fe6cbf6a3f5554db543a8abd7f0e984
Brandeau <diego.andrade.y@gmail.com>
2022-10-20 12:56:16 -0300

    add other clocks

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 function displayDate() {
   const today = new Date();
-  document.getElementById("date").textContent = today.toLocaleDateString("sv");
+  document.getElementById("chile-date").textContent =
+    today.toLocaleDateString("sv");
 
-  document.getElementById("time").textContent = today.toLocaleTimeString(
+  document.getElementById("chile-time").textContent = today.toLocaleTimeString(
     "GMT-0400",
     { hourCycle: "h24" }
+  );
+
+  document.getElementById("buddhist-date").textContent =
+    today.toLocaleDateString("th-TH");
+
+  document.getElementById("buddhist-time").textContent =
+    today.toLocaleTimeString("sv", {
+      timeZone: "Asia/Bangkok",
+      hourCycle: "h24",
+    });
+
+  document.getElementById("japan-date").textContent = today.toLocaleDateString(
+    "ja-JP-u-ca-japanese",
+    { era: "long" }
+  );
+
+  document.getElementById("japan-time").textContent = today.toLocaleTimeString(
+    "ja-JP",
+    {
+      timeZone: "Asia/Tokyo",
+      hourCycle: "h24",
+    }
   );
 }
 
